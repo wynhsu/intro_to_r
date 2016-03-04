019bb62354fc0b8e23f2f14713b38c0090b766b7
unknown <abertozz@IHMELT191.ihme.washington.edu>
2016-03-03 23:22:25 -0800

    quick updates to exercise code (for consistency with response)

--- ggplot/ggplot_exercise.r
+++ ggplot/ggplot_exercise.r
@@ -6,24 +6,27 @@ library(dplyr)
 
 load("suicides.rdata")
 
+suicides$age <- as.factor(suicides$age)
+
 all_suicides <- copy(suicides)
 suicides <- suicides %>% 
-  group_by(year, state, age) %>% 
+  group_by(year, age, sex) %>% 
   mutate(deaths = sum(deaths))
 
 #  Make a line plot of suicides by age
 # (year on the x axis, deaths on the y axis, different line for each age).
-
+# facet by sex.
 
 
 ##extra credit####
 
-one_state <- all_suicides[all_suicides$state=="Bihar"] %>% 
-  group_by(year, state, sex, age) %>% 
+one_state <- all_suicides[all_suicides$state=="Uttar Pradesh"] %>% 
+  group_by(year, state, sex, age, means) %>% 
   mutate(deaths = sum(deaths))
 
 # Make a set of density plots faceted by sex and means of suicide,
-# showing distributions of suicides by age, for the state of Bihar.
+# showing distributions of suicides by age, for the state of Uttar Pradesh.
+# Label appropriately.
 
 
 
